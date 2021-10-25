8941de3d0312b51c891718282b664b416bcfaf4d
Zakwan105 <khairulzakwan105@gmail.com>
2021-10-25 12:56:42 +0800

    password

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelbagai Soalan</title>
+</head>
+<form action="process.php" method="post">
+    Nama
+    <textarea name="nama"></textarea>
+    <br>
+    Password
+    <input type="password" name="kata">
+    <button type="submit">Go</button>
+
+</form>
+
+<body>
+</body>
+
+</html>
